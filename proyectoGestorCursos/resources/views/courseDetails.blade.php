d0974a9e3bcfecbbcd5b6ee82d9569405e23c26e
Jaime <jaimemoralmillan@digitechfp.com>
2024-11-04 09:44:25 +0100

    vista estudiantes avanzada
    lista todos los cursos, y se muestran los detalles de cada uno al clicarle

---
+++ proyectoGestorCursos/resources/views/courseDetails.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    
+    <h1>{{$course->title}} <h1>
+    <h2>Descripcion:</h2>
+    <p>{{$course->description}}</p>
+    <h2>Curriculum:</h2>
+    <p>{{$course->curriculum}} </p>
+
+    
+</body>
+</html>
--- proyectoGestorCursos/routes/web.php
+++ proyectoGestorCursos/routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\WebController;
 Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
     Route::get('/',[WebController::class,'dashboard'])->name('dashboard'); // Jaime
     Route::get('/teachers',[TeacherController::class,'teacherCourses'])->name('teacherCourses'); // Felipe Vista Profesores
-    // Route::delete('/teachers/{id}',[TeacherController::class,'destroyCourses'])->name('destroyCourses'); //Felipe Boton para borrar un curso
+    Route::delete('/teachers/{id}',[TeacherController::class,'destroyCourses'])->name('destroyCourses'); //Felipe Boton para borrar un curso
     Route::get('/teachers/courses/{id}/edit',[TeacherController::class,'edit'])->name('editCourses'); // Angela boton que te lleva a la vista editar
     Route::put('/teachers/update/{id}',[TeacherController::class,'updateCourses'])->name('updateCourses'); // Ángela   Ruta que edita el curso con nueva info
     Route::get('/teachers/create',[TeacherController::class,'create'])->name('create');//Angela ruta que te lleva al formulario donde introduces los datos del nuevo curso
@@ -23,10 +23,8 @@ Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])
 
     
     Route::get('/students',[StudentController::class,'students'])->name('students'); //Jaime ruta a vista inicial de students
-    Route::get('/students/courseDetails',[StudentController::class,'courseDetails'])->name('courseDetails'); //Jaime ruta que lleva a los detalles y contenido del curso clicado
+    Route::get('/students/courseDetails{id}',[StudentController::class,'courseDetails'])->name('courseDetails'); //Jaime ruta que lleva a los detalles y contenido del curso clicado
     Route::get('/students/availableCourses',[StudentController::class,'availableCourses'])->name('availableCourses'); //Jaime ruta que lleva al listado de todos los cursos creados
-
-
 
    
 });
